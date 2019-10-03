70bb6da285058ce7cb91305590d888caffb184ce
Piyush2769 <startcheckingyourmail@gmail.com>
2019-10-03 16:16:55 +0530

    Inirial+1

--- app/src/main/java/com/piyushmaheswari/everythingandroid/Java/JavaActivity.java
+++ app/src/main/java/com/piyushmaheswari/everythingandroid/Java/JavaActivity.java
@@ -3,6 +3,8 @@ package com.piyushmaheswari.everythingandroid.Java;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.piyushmaheswari.everythingandroid.R;
 import com.ramotion.foldingcell.FoldingCell;
@@ -13,6 +15,9 @@ public class JavaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_java);
+
+        TextView d;
+        ImageView s;
 
     }
 }
